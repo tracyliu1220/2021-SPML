b02b8b7f83dc2ca02810a877d858f732163a28a5
Tracy Liu <tracyliu981220@gmail.com>
2021-12-24 15:16:24 +0800

    feat: Epsilons for H, S, V

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import torchvision
 import torchvision.transforms as transforms
 from fgsm import FGSM
 from convertor import Convertor
+from torchvision.utils import save_image
 
 device = torch.device("cuda" if (torch.cuda.is_available()) else "cpu")
 
 # ==== parameters ====
-eps = 0.1
+eps = torch.Tensor([0.3, 0.15, 0.2]).to(device)
 batch_size = 4
 
 # ==== model ====
@@ -27,22 +28,33 @@ trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True,
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,shuffle=False, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,shuffle=True, num_workers=2)
 
 correct = 0
 total = 0
 
 for i,data in enumerate(testloader):
-    if i == 500:
-        break
+    # if i == 500:
+    #     break
     images, labels = data
     images, labels = images.to(device), labels.to(device)
 
+    save_image(images[0], 'test.png')
+    if i == 0:
+        print(labels[0])
+
     # generate adversarial images
     adv_imgs = attack.gen_adv(images,labels)
+    if i == 0:
+        save_image(adv_imgs[0], 'test_adv.png')
 
     # go through the target model
+    if i == 0:
+        outputs = model(convertor.normalize(images))
+        print('origin:', torch.max(outputs.data, 1)[1][0])
     outputs = model(convertor.normalize(adv_imgs))
+    if i == 0:
+        print('adv:', torch.max(outputs.data, 1)[1][0])
     
     # the class with the highest energy is what we choose as prediction
     _, predicted = torch.max(outputs.data, 1)
